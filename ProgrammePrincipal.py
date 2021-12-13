5e5e38ef00e8cb83fef48889f77d5a21a665fb72
Amaury Chronowski <amaury.chronowski@cpe.fr>
2021-12-13 14:12:53 +0100

    Début

---
+++ ProgrammePrincipal.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Header
+
+"""
+Programme principale Sapce Invader
+
+Amaury CHRONOWSKI/Gabin JOBERT--ROLLIN
+
+15/11/2021
+
+Réalise le lancement du Space Invader
+
+
+
+-------
+
+"""
+#Importation de bibliothèques nécessaires
+from ProgrammeSecondaire import SpaceInvader
+
+#Programme principal
+if __name__ == "__main__":
+    Inv = SpaceInvader()
+    Inv.mainloop()
---
+++ ProgrammeSecondaire.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Header
+
+"""
+Programme secondaire Sapce Invader
+
+Amaury CHRONOWSKI/Gabin JOBERT--ROLLIN
+
+15/11/2021
+
+Réalise le lancement du Space Invader
+
+
+
+-------
+
+"""
