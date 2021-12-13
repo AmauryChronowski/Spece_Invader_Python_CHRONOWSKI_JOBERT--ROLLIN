3eaaffc5d7b8ed7acf6465e1e357743b9691b6eb
Amaury Chronowski <amaury.chronowski@cpe.fr>
2021-12-13 14:32:53 +0100

    Début Menu

--- ProgrammeSecondaire.py
+++ ProgrammeSecondaire.py
@@ -17,7 +17,7 @@ Réalise le lancement du Space Invader
 
 """
 #Importation de bibliothèques nécessaires
-from tkinter import Tk
+from tkinter import Tk, Frame, Button
 
 class SpaceInvader(Tk):
     def __init__(self):
@@ -28,6 +28,10 @@ class SpaceInvader(Tk):
         self.geometry('1600x900')
         
         # Menu
+        self.Menu = Frame()
+        self.Menu.pack()
+        self.greenbutton = Button(self.Menu, text="Brown", fg="brown",command=self.Menu.destroy)
+        self.greenbutton.pack()
         
         
         
