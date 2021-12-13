6fac7829af79a7af5ec011d0f6c4925afe487276
Amaury Chronowski <amaury.chronowski@cpe.fr>
2021-12-13 15:40:41 +0100

    Fin Menu

--- ProgrammeSecondaire.py
+++ ProgrammeSecondaire.py
@@ -17,7 +17,7 @@ Réalise le lancement du Space Invader
 
 """
 #Importation de bibliothèques nécessaires
-from tkinter import Tk, Frame, Button
+from tkinter import Tk, Frame, Button,Label
 
 class SpaceInvader(Tk):
     def __init__(self):
@@ -28,11 +28,14 @@ class SpaceInvader(Tk):
         self.geometry('1600x900')
         
         # Menu
-        self.Menu = Frame()
-        self.Menu.pack()
-        self.greenbutton = Button(self.Menu, text="Brown", fg="brown",command=self.Menu.destroy)
-        self.greenbutton.pack()
-        
+        self.Menu = Frame(bg="black")
+        self.titre=Label(self.Menu, text="Sapce Invader",font=("Helvetica",80), fg="green",bg="black")
+        self.titre.pack(side="top", expand="yes")
+        self.Démarrer = Button(self.Menu, text="START",font=("Helvetica",30),fg="green",bg="black",activebackground='green',activeforeground='white',highlightcolor="white",highlightthickness=4,relief="flat",highlightbackground="green",command=self.startPartie)
+        self.Démarrer.pack(side="top", expand="yes")
+        self.Quitter = Button(self.Menu, text="QUIT",font=("Helvetica",30),fg="green",bg="black",activebackground='green',activeforeground='white',highlightcolor="white",highlightthickness=4,relief="flat",highlightbackground="green",command=self.destroy)
+        self.Quitter.pack(side="top", expand="yes")
+        self.Menu.pack(fill="both",expand="yes") 
         
         
     
